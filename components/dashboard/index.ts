8484cf6a013e9e31b13e8aaf0ead53e67f71cb6f
cari1412 <vadis1766@gmail.com>
2026-01-09 12:12:23 +0300

    fix z

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,4 +1,3 @@
-export { default as DashboardView } from './DashboardView';
 export { default as PinterestConnect } from './PinterestConnect';
 export { default as CreatePinForm } from './CreatePinForm';
 export { default as CreateBoardModal } from './CreateBoardModal';
