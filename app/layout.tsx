d8ab6beac2420f56a5bf6ec244ab3116f5ba227f
cari1412 <vadis1766@gmail.com>
2026-02-17 20:05:09 +0300

    gt

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PXB6S52Z');
+            })(window,document,'script','dataLayer','GTM-MVKRGX6W');
           `}
         </Script>
         
@@ -38,7 +38,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PXB6S52Z"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MVKRGX6W"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
